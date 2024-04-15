79a6f8e1d78fa4479453f4b41c7a60f06ec0f5fc
krenarh <krenarhazari@gmail.com>
2024-04-15 15:01:08 +0200

    gonna update later

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import './App.css';
-import {citiesData} from './constants/cityTemperatures'
+import { useState } from "react";
+import "./App.css";
+import { citiesData } from "./constants/cityTemperatures";
+import CustomButton from "./components/CustomButton";
 
 function App() {
+  const [cityTemperature, setCityTemperature] = useState(citiesData);
+  function handleTChange(index, change) {
+    const updatedTemperatures = [...cityTemperature];
+    updatedTemperatures[index].temperature += change;
+    setCityTemperature(updatedTemperatures);
+  }
   return (
     <div>
       <ul>
-      {citiesData.map((c)=>
-      <p key={c.city}>{c.city} - {c.temperature} </p>)}
+        {citiesData.map((c, index) => (
+          <div key={index}>
+          <p>
+            {c.city} -  <CustomButton title="-" onSelect={() => handleTChange(index, -1)} />  {c.temperature} <CustomButton title="+" onSelect={() => handleTChange(index, 1)} />
+          </p>
+          </div>
+        ))}
       </ul>
     </div>
   );
