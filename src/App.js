e723b0d4e683e0012c5c2fb0927bdb95ce59e27a
krenarh <krenarhazari@gmail.com>
2024-04-15 13:35:29 +0200

    Adding and displaying data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import {citiesData} from './constants/cityTemperatures'
 
 function App() {
   return (
     <div>
-      
+      <ul>
+      {citiesData.map((c)=>
+      <p key={c.city}>{c.city} - {c.temperature} </p>)}
+      </ul>
     </div>
   );
 }
---
+++ src/constants/cityTemperatures.js
@@ -0,0 +1,7 @@
+export const citiesData = [
+    {city: "New York", temperature: 55},
+    {city: "Tokyo", temperature: 68},
+    {city: "London", temperature: 48},
+    {city: "Paris", temperature: 52},
+    {city: "Dubai", temperature: 85}
+];
